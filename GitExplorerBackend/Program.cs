3d037e1c6746dc829d00c0f0afde3cb08e197217
icedid <94460146+icedid@users.noreply.github.com>
2026-05-10 01:13:28 +1200

    bug/appsetting mistype

--- GitExplorerBackend/Program.cs
+++ GitExplorerBackend/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddControllers();
 
 // --- Database ---
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // --- Session ---
 builder.Services.AddDistributedMemoryCache();
